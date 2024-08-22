a164a308660d4a162b8b8a70193c567aaffdec42
jose.morais <jose.morais@dbseller.com.br>
2024-08-22 16:02:42 -0300

    M28187 - trait 1280

--- src/Factories/Traits/TraitS1280.php
+++ src/Factories/Traits/TraitS1280.php
@@ -132,7 +132,7 @@ trait TraitS1280
         $this->eSocial->appendChild($this->node);
         $this->sign();
     }
-    
+
     /**
      * builder for version S.1.0.0
      */
@@ -140,7 +140,7 @@ trait TraitS1280
     {
          throw new \Exception("TODO !!");
     }
-    
+
     /**
      * builder for version S.1.1.0
      */
@@ -148,4 +148,115 @@ trait TraitS1280
     {
          throw new \Exception("TODO !!");
     }
+
+    protected function toNodeS120()
+    {
+        $ideEmpregador = $this->node->getElementsByTagName('ideEmpregador')->item(0);
+
+
+        $ideEvento = $this->dom->createElement("ideEvento");
+        $this->dom->addChild(
+            $ideEvento,
+            "indRetif",
+            $this->std->indretif,
+            true
+        );
+
+        $this->dom->addChild(
+            $ideEvento,
+            "nrRecibo",
+            ! empty($this->std->nrrecibo) ? $this->std->nrrecibo : null,
+            false
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "indApuracao",
+            $this->std->indapuracao,
+            false
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "perApur",
+            $this->std->perapur,
+            false
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "tpAmb",
+            $this->tpAmb,
+            true
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "procEmi",
+            $this->procEmi,
+            true
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "verProc",
+            $this->verProc,
+            true
+        );
+        $this->node->insertBefore($ideEvento, $ideEmpregador);
+
+        if (isset($this->std->infosubstpatr)) {
+            $infoSubstPatr = $this->dom->createElement("infoSubstPatr");
+
+            $this->dom->addChild(
+                $infoSubstPatr,
+                "indSubstPatr",
+                $this->std->infosubstpatr->indsubstpatr,
+                true
+            );
+
+            $this->dom->addChild(
+                $infoSubstPatr,
+                "percRedContrib",
+                $this->std->infosubstpatr->percredcontrib,
+                true
+            );
+
+            $this->node->appendChild($infoSubstPatr);
+        }
+
+        if (isset($this->std->infosubstpatropport)) {
+            foreach ($this->std->infosubstpatropport as $info) {
+                $infoSubstPatrOpPort = $this->dom->createElement("infoSubstPatrOpPort");
+                $this->dom->addChild(
+                    $infoSubstPatrOpPort,
+                    "cnpjOpPortuario",
+                    $info->codlotacao,
+                    true
+                );
+
+                $this->node->appendChild($infoSubstPatrOpPort);
+            }
+        }
+
+        if (isset($this->std->infoativconcom)) {
+            $infoAtivConcom = $this->dom->createElement("infoAtivConcom");
+
+            $this->dom->addChild(
+                $infoAtivConcom,
+                "fatorMes",
+                $this->std->infoativconcom->fatormes,
+                true
+            );
+
+            $this->dom->addChild(
+                $infoAtivConcom,
+                "fator13",
+                $this->std->infoativconcom->fator13,
+                true
+            );
+
+            $this->node->appendChild($infoAtivConcom);
+        }
+
+        $this->eSocial->appendChild($this->node);
+
+        $this->sign();
+    }
+
 }
